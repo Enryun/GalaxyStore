c2c0d2e14b7afd01b39728d106fb6efea9e9a991
JamesThang <ext.dagt@fpt.com>
2024-03-09 15:41:49 +0700

    Initial Commit

---
+++ GalaxyStore/GalaxyStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalaxyStoreApp.swift
+//  GalaxyStore
+//
+//  Created by ThangDDB on 09/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct GalaxyStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
